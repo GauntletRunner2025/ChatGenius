494cfa8a761ef50f28796011a5a09c41497f42d2
GauntletRunner2025 <gauntletrunner2025@gauntletai.com>
2025-01-17 02:10:07 -0600

    layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from '../Header';
 import styles from './Layout.module.css';
 import ChannelList from '../Channel/ChannelList';
+import { UsersList } from '../UsersList';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,15 +11,15 @@ interface LayoutProps {
 export function Layout({ children }: LayoutProps) {
   return (
     <div className={styles.container}>
-      <div className={styles.sidebar}>
+      <div className={styles.channels}>
         <div className={styles.logo}>ChatGenius</div>
-        <nav className={styles.navigation}>
-          <ChannelList />
-        </nav>
+        <ChannelList />
       </div>
-      <div className={styles.main}>
-        <Header />
+      <main className={styles.main}>
         {children}
+      </main>
+      <div className={styles.users}>
+        <UsersList />
       </div>
     </div>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ChannelList from '../components/Channel/ChannelList';
+import { MainContent } from '../components/MainContent';
 
 const MainPage: React.FC = () => {
   return (
     <div className="w-full h-full">
-      <h1>Main Dashboard</h1>
-      <ChannelList />
+      <MainContent />
     </div>
   );
 };
